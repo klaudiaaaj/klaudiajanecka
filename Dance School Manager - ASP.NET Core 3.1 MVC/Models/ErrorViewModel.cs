38fc6056d54301d584843d55871f393cc7dfca38
klaudiaaaj <klaudiaaa.janecka@gmail.com>
2020-12-23 22:58:04 +0100

    remove

--- Dance School Manager - ASP.NET Core 3.1 MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DanceSchool_10._05_ASP.NET_MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Dance School Manager - ASP.NET Core 3.1 MVC/html/search/all_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['efmigrationshistory_7',['Efmigrationshistory',['../class_dance_school__10_1_1__05___a_s_p_1_1_n_e_t___m_v_c_1_1_models_1_1_efmigrationshistory.html',1,'DanceSchool_10::_05_ASP::NET_MVC::Models']]],
-  ['errorviewmodel_8',['ErrorViewModel',['../class_dance_school__10_1_1__05___a_s_p_1_1_n_e_t___m_v_c_1_1_models_1_1_error_view_model.html',1,'DanceSchool_10::_05_ASP::NET_MVC::Models']]]
-];
